31d82fe27cb05a6bf4fee7d59a3efa81359a2293
ikedayohei <yoheii0612@gmail.com>
2020-04-24 22:09:56 +0900

    バリデーション

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,6 +5,14 @@ class Company < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
+  with_options presence: true do
+    validates :name
+    validates :postcode
+    validates :phone_number
+    validates :capital
+    validates :sale
+    validates :description
+
   def self.search(input)
     return nil if input == ""
     Company.where(['name LIKE(?) or postcode LIKE(?) or description LIKE(?) or characteristic LIKE(?)', "%#{input}%","%#{input}%" ,"%#{input}%" ,"%#{input}%"] ).limit(5)
